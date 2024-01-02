dc5e5ed8ee1a09be78dd92ec6a6d026d088d8127
Gizmo <anvaradizov100@gmail.com>
2024-01-02 19:52:41 +0500

    fetching data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://app-ryx4.vercel.app');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data && data}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 
+app.use(cors())
 app.get('/', (req,res) => {
-    res.json('hello world')
+    res.send('hello world')
 })
 
 app.listen(3001)

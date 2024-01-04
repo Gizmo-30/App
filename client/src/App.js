3f8b698a7776913c3700d882dd287dfa3f641c7e
Anvar Adizov <anvaradizov100@gmail.com>
2024-01-04 16:31:43 +0500

    created login and registration pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Test2 from "./components/Test2";
-import Test1 from "./components/Test1";
+import Registration from "./components/Registration";
+import LogIn from "./components/LogIn";
 
 import axios from "axios";
 axios.defaults.baseURL = "https://collections-server.vercel.app";
+
 function App() {
   return (
       <BrowserRouter>
             <div>
                 <Routes>
-                    <Route path="/" element={<Test1 />}></Route>
-                    <Route path="/test2" element={<Test2 />}></Route>
+                    <Route path="/" element={<LogIn />}></Route>
+                    <Route path="/registration" element={<Registration />}></Route>
                 </Routes>
             </div>
       </BrowserRouter>

e643991a32e3d04b5f4449bf9ff4c074db673dbe
Anvar Adizov <anvaradizov100@gmail.com>
2024-01-06 20:51:45 +0500

    set sequelize and creating rows

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const path = require('path');
 const router = require("./routes/router");
 const app = express()
+const db = require("./models")
+const {Users} = require("./models")
+console.log(Users)
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const PORT = process.env.PORT || 4000;
@@ -13,13 +16,29 @@ const PORT = process.env.PORT || 4000;
 app.use(cors())
 app.use('/users', router)
 app.get('/', (req,res) => {
-    res.send('hello world')
+    res.send(`<div>
+       hello world  
+       <a href="/users">users</a>
+       <a href="/test">test</a>
+       </div>`)
+})
+app.get('/test', async (req,res) => {
+    try {
+        // await Users.create({
+        //     username: 'adsdf',
+        //     email: 'asdffsdff@gmail.com',
+        //     password: '123',
+        //     role: 'user',
+        //     status: 'active',
+        // })
+        res.send('its working')
+    } catch (err) {
+        console.log(err)
+    }
+})
+db.sequelize.sync().then((req) => {
+    app.listen(PORT, (req,res) => {
+        console.log(`Server running at http://localhost:${PORT}`)
+    })
 })
 
-app.get('/test', (req,res) => {
-    res.send('its working')
-})
-
-app.listen(PORT, (req,res) => {
-    console.log(`Server running at http://localhost:${PORT}`)
-})
---
+++ server/models/Users.js
@@ -0,0 +1,37 @@
+const {DataTypes} = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    const Users = sequelize.define("Users", {
+        username: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: {
+                arguments: true,
+                message: "duplicate"
+            },
+            defaultValue: '',
+        },
+        email: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            unique: true,
+            defaultValue: '',
+        },
+        password: {
+            type: DataTypes.CHAR(60),
+            allowNull: false,
+        },
+        role: {
+            type: DataTypes.ENUM('admin', 'user'),
+            allowNull: false,
+            defaultValue: "user",
+        },
+        status: {
+            type: DataTypes.ENUM('block', 'active'),
+            allowNull: false,
+            defaultValue: "active",
+        },
+    })
+    Users.sync({ alter: true })
+    return Users
+}
+

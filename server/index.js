3ba207eba7aa5bca9a134f4d1519a1f6b636360a
Anvar Adizov <anvaradizov100@gmail.com>
2024-01-08 01:22:23 +0500

    set up config/config.js insted config/config.json

--- server/index.js
+++ server/index.js
@@ -8,17 +8,18 @@ const {Sequelize} = require("sequelize");
 const mysql2 = require('mysql2');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const PORT = process.env.PORT || 4000;
-const db = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: "mysql",
-        dialectModule: mysql2,
-        host: process.env.DB_HOST,
-        port: 3306,
-    }
-)
+
+// const db = new Sequelize(
+//     process.env.DB_DATABASE,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: "mysql",
+//         dialectModule: mysql2,
+//         host: process.env.DB_HOST,
+//         port: 3306,
+//     }
+// )
 
 //
 //
@@ -29,7 +30,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use('/', router)
 
-db.sync({alter: true}).then((req) => {
+sequelize.sync({alter: true}).then((req) => {
     app.listen(PORT, (req,res) => {
         console.log(`Server running at http://localhost:${PORT}`)
     })

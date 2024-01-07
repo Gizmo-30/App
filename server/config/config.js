10a2873784b8009a06a4c66c9f8a7598f2b9d795
Anvar Adizov <anvaradizov100@gmail.com>
2024-01-08 01:36:45 +0500

    add dialect option config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,29 +1,44 @@
 const path = require("path");
 const mysql2 = require("mysql2");
+const fs = require('fs');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql",
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
     dialectModule: mysql2,
+    dialectOptions: {
+      bigNumberStrings: true,
+      ssl: {
+        ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
+      }
+    }
   },
-  "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql",
-    dialectModule: mysql2,
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true
+    }
   },
-  "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql",
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
     dialectModule: mysql2,
+    dialectOptions: {
+      bigNumberStrings: true,
+      ssl: {
+        ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
+      }
+    }
   }
 }

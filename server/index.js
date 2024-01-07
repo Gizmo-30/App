f8e8e770d39c5197a014ed8918e3f691fc49996c
Anvar Adizov <anvaradizov100@gmail.com>
2024-01-08 01:49:36 +0500

    remove connection from index.js

--- server/index.js
+++ server/index.js
@@ -9,17 +9,17 @@ const mysql2 = require('mysql2');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const PORT = process.env.PORT || 4000;
 
-const db = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: "mysql",
-        dialectModule: mysql2,
-        host: process.env.DB_HOST,
-        port: 3306,
-    }
-)
+// const db = new Sequelize(
+//     process.env.DB_DATABASE,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: "mysql",
+//         dialectModule: mysql2,
+//         host: process.env.DB_HOST,
+//         port: 3306,
+//     }
+// )
 
 //
 //

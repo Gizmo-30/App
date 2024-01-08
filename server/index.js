6772952738d203eff39ede116cb3213f1927c560
Anvar Adizov <anvaradizov100@gmail.com>
2024-01-08 16:15:46 +0500

    db

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const cors = require('cors');
 const path = require('path');
 const router = require("./routes/router");
 const bodyParser = require('body-parser');
-const {db, User} = require("./models");
+const db = require("./models");
+const {User} = require("./models");
 const {Sequelize} = require("sequelize");
 const mysql2 = require('mysql2');
-// const db = require("./db");
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const PORT = process.env.PORT || 4000;
 
@@ -19,7 +19,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 try{
-    db.sequelize.sync()
+    db.sequelize.sync().then(r => console.log(r)).catch(e => console.error(e))
     app.listen(PORT, (req,res) => {
         console.log(`Server running at http://localhost:${PORT}`)
     })
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {sequelize, User} = require("../models");
+const {User} = require("../models");
 
 router.get('/', (req,res) => {
     res.send(`<div>

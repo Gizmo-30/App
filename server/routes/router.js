a466d3e39dce1624e49f717eae8a42793a4ab56d
Anvar Adizov <anvaradizov100@gmail.com>
2024-01-08 16:21:53 +0500

    sync in router

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {User} = require("../models");
+const db = require("../models");
 
 router.get('/', (req,res) => {
     res.send(`<div>
@@ -8,6 +9,12 @@ router.get('/', (req,res) => {
        <a href="/users">users</a>
        </div>`)
 })
+
+try{
+    db.sequelize.sync().then(r => console.log(r)).catch(e => console.error(e))
+} catch (e) {
+    console.error('Unable to connect to the database --->', e);
+}
 
 router.get('/users', async (req,res) => {
     try{

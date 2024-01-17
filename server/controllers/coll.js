dcaa4d1fbeabd71fff539a9495017ca51ce08e5f
Anvar Adizov <anvaradizov100@gmail.com>
2024-01-17 11:28:59 +0500

    get collections with user succeess

--- server/controllers/coll.js
+++ server/controllers/coll.js
@@ -1,4 +1,9 @@
+const {Collections, User} = require("../models")
 exports.createColl = (req,res) => {
     const {name, description, type, user} = req.body
     console.log(name, description, type, user)
+}
+exports.getColl = async (req,res) => {
+    const response = await Collections.findAll({include: [User]})
+    res.json(response)
 }
--- server/routes/coll.js
+++ server/routes/coll.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const {createColl} = require("../controllers/coll");
+const {createColl, getColl} = require("../controllers/coll");
 const coll = express.Router()
 
 coll.post('/coll/create', createColl)
+coll.get('/coll/get', getColl)
 
 module.exports = coll

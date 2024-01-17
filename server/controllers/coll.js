4fca46518741ba353de0948350deeebdfef408c4
Anvar Adizov <anvaradizov100@gmail.com>
2024-01-17 11:37:53 +0500

    finind collections with same names

--- server/controllers/coll.js
+++ server/controllers/coll.js
@@ -1,8 +1,14 @@
 const {Collections, User} = require("../models")
-exports.createColl = (req,res) => {
+exports.createColl = async (req,res) => {
     const {name, description, type, user} = req.body
-    console.log(name, description, type, user)
+    const searchResult = await Collections.findAll({where: {name,}})
+    console.log(searchResult)
 }
+
+
+
+
+
 exports.getColl = async (req,res) => {
     const response = await User.findByPk(1,{include: [Collections]})
     res.json(response)
